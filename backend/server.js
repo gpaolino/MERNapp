25934ca70134933adca16f70614b10db8abe8308
gpaolino <gp.guglielminopaolo@me.com>
2022-10-01 17:58:27 +0200

    Added example GET request and moved parameters to dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // express app
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
 // listen for requests
-app.listen(4000, () => {
-    console.log('listening on port 4000')
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
 })
