aed201198a388d13e44dda473fb3b6170a70fc10
gpaolino <gp.guglielminopaolo@me.com>
2022-10-02 23:10:23 +0200

    added routes and connection to mongo db database

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,31 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const vehicleRoutes = require('./routes/vehicles')
 
 // express app
 const app = express()
 
 //middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/vehicles', vehicleRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
